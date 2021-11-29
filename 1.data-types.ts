12ca1adbf76cddb2ae33384d898a7fa855f96335
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-29 13:27:34 +0500

    Data types in TS

---
+++ 1.data-types.ts
@@ -0,0 +1,15 @@
+// declaration_type variable_name: data_type = value;
+
+// Type number
+let weight: number = 100;
+let age = 23; // weight and age are of type number;
+
+// Type string
+let myName = 'Zain Sadaqat';
+// let myPetName: string = "I don't have any pet";
+// myPetName = "I don't have any pet".length;  because .length returns a number
+// myPetName = "I don't have any pet".length === 0;  because === returns a boolean
+
+// Type boolean
+let isAlive = true;
+isAlive = "I don't have any pet".length > 10; 
